c55bfb467cc42cae8c86ab5b06e978994bdb8329
Plinio <pliniosmarini@ufu.br>
2021-05-26 14:01:05 -0300

    Transação binário

--- transacao/DadosTransacao.java
+++ transacao/DadosTransacao.java
@@ -1,16 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.io.Serializable;
 
 class DadosTransacao{
 	private ArrayList<Transacao> vetTrans = new ArrayList<Transacao>();
-	public void cadastrar(Transacao c) {
-		this.vetTrans.add(c); //ADICIONA A TRANSAÇÃO NO ARRAY
-		System.out.println("Total de transacões inseridas: ");
+	public void add(Transacao c) {
+		this.vetTrans.add(c); //ADICIONA A TRANSAï¿½ï¿½O NO ARRAY
+		System.out.println("Total de transacï¿½es inseridas: ");
 		System.out.println(this.vetTrans.size());
 	} public void listar(){
 		mostrarDados();
 	}
-	//este método retorna o objeto Transacao caso encontrado, ou null, caso não encontrado
-	public Transacao buscar(int nro) {//pode-se usar também int
+	//este mï¿½todo retorna o objeto Transacao caso encontrado, ou null, caso nï¿½o encontrado
+	public Transacao busca(int nro) {//pode-se usar tambï¿½m int
 		Transacao c = null;
 			for ( Transacao objeto: this.vetTrans) {
 				if (objeto.getNroContrato() == (nro)) {
@@ -20,7 +29,7 @@ class DadosTransacao{
 			} return c;
 	}
 	
-	public boolean excluir(int nro){
+	public boolean exclui(int nro){
 		Transacao c = this.buscar(nro);
 		if (c != null) {
 				this.vetTrans.remove(c);
@@ -35,4 +44,52 @@ class DadosTransacao{
             System.out.println( objeto.toString());
         }
     }
+
+	public void salva() {
+		BufferedWriter escritor = null;
+		
+		if(!vetTrans.isEmpty()) {
+			try {
+				escritor = new BufferedWriter(new FileWriter(new File("transacao"))); 
+				for(Transacao trans : vetTrans) {
+					escritor.write(trans.toString());
+				}
+				escritor.flush();
+				escritor.close();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+    } // ----------- salva -----------//
+
+        // ----------- salvaBin -----------//
+        public void salvaBin(){
+        FileOutputStream escritorArq = null; 
+		ObjectOutputStream escritorObj = null; 
+		
+		if(!vetTrans.isEmpty()) {
+			try {
+				escritorArq = new FileOutputStream("transacaobin"); 
+				escritorObj = new ObjectOutputStream(escritorObj); 
+				escritorObj.writeObject(vetTrans);
+			}
+			catch(FileNotFoundException e) {
+				System.out.println(e.getMessage());
+			}
+			catch(IOException e) {
+				System.out.println(e.getMessage());
+			}
+			finally {
+				try {
+					if(escritorArq != null)
+						escritorArq.close(); 
+				}
+				catch(Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		}
+    }
+        // ----------- salvaBin -----------//
 }
